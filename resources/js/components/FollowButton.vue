01808f827fec38e30cf48a285eef9e185aa59c8a
sophie <s.hautcoeur@gmail.com>
2020-04-08 10:49:21 +0300

    tweaked ui and added pgsql db

--- resources/js/components/FollowButton.vue
+++ resources/js/components/FollowButton.vue
@@ -24,8 +24,6 @@
                 axios.post('/follow/' + this.userId)
                     .then(response => {
                         this.status = ! this.status;
-
-                        console.log(response.data);
                     })
                     .catch(errors => {
                         if (errors.response.status == 401) {
